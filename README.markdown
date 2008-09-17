72b916a3e04a90891db27bebc9fa23761e2c1580
Yehuda Katz <wycats@gmail.com>
2008-09-17 18:04:56 +0100

    Initial commit (contains a patched flying saucer to support :last-child)

---
+++ README.markdown
@@ -0,0 +1,5 @@
+UFO
+===
+
+A gem that provides a Ruby binding to the flying saucer Java library for creating PDFs out
+of HTML and CSS.
---
+++ lib/flying_saucer.rb
@@ -0,0 +1,7 @@
+require "java"
+require "flying_saucer/core-renderer"
+require "flying_saucer/itext_renderer"
+
+import org.jruby.util.IOOutputStream
+
+java.lang.System.set_property("java.awt.headless", "true")
